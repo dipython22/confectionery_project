1cc6dcebf771fa5b22c1f5eea99b7bcc8fa87475
Vada2022 <whitesnail2022@gmail.com>
2022-06-29 15:00:41 +0300

    done for now

--- confectionery/tortukai/admin.py
+++ confectionery/tortukai/admin.py
@@ -1,36 +1,25 @@
 from django.contrib import admin
 from .models import Occasion, Cake, Order, CakeReview, OrderLine
-
     
-# class ClientAdmin(admin.ModelAdmin):
-#     list_display = ('first_name', 'last_name', 'email_name', 'phone_number',)
-    
-
-
 
 class CakeAdmin(admin.ModelAdmin):
     list_display = ('cake_name', 'description', 'occasion', 'price',)
     list_filter = ('occasion__title', )
     list_editable = ('price',)
 
-class OrderLineInline(admin.StackedInline): ## Padaryti, kad užsakymo formoje būtų matomos ir užsakymo eilutės (į jas galima būtų įrašyti informaciją)
+class OrderLineInline(admin.StackedInline): 
     model = OrderLine
     can_delete = False
     extra = 0
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = ('order_date', 'total_price', 'client_car', 'status'  )
-#     inlines = (OrderLineInline, ) ## Padaryti, kad užsakymo formoje būtų matomos ir užsakymo eilutės (į jas galima būtų įrašyti informaciją)
-#     list_editable = ('status',)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'deadline', 'status', 'customer', 'order_total_price', 'notice',) #'display_cakes'
-    inlines = (OrderLineInline, ) ## Padaryti, kad užsakymo formoje būtų matomos ir užsakymo eilutės (į jas galima būtų įrašyti informaciją)
+    list_display = ('id', 'deadline', 'status', 'customer', 'order_total_price', 'notice',) 
+    inlines = (OrderLineInline, ) 
     list_filter = ('status', 'deadline',)
     search_fields = ('id', 'customer__username',)
     readonly_fields = ('id', )
     list_editable = ('deadline', 'status','customer',) 
-    # list_display_links = ('deadline', 'status')
 
     fieldsets = (
         ('Pagrindinė Informacija', {'fields': (
@@ -51,8 +40,7 @@ class OrderLineAdmin(admin.ModelAdmin):
     list_filter = ('cake', 'order__deadline', )
     search_fields = ('order__username', )
     readonly_fields = ('line_created', )
-    list_editable = ('cake', 'quantity', 'discount', ) #,'customer',) 
-
+    list_editable = ('cake', 'quantity', 'discount', )
 
 admin.site.register(Occasion)
 admin.site.register(Cake, CakeAdmin)
